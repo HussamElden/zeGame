250739f982430f6f81697815e75f89e45ce055c3
Ahmed ElKerdany <32311812+Akerdany@users.noreply.github.com>
2017-11-21 17:55:25 +0200

    Soldier Spaces

--- src/project/s/classes/Soldier.java
+++ src/project/s/classes/Soldier.java
@@ -13,47 +13,54 @@ import java.util.ArrayList;
  * @author Ahmed
  */
 public class Soldier extends Movables implements  Ibehavior{
-  private  Melee sword=new Melee();
-    private static int Number=0;
-     PriceLists price=new PriceLists();
-    static public ArrayList<Soldier>ArrSoldier=new ArrayList<>();
+    private  Melee sword = new Melee();
+    private static int Number = 0;
+    PriceLists price = new PriceLists();
+    static public ArrayList<Soldier>ArrSoldier = new ArrayList<>();
 
-    Soldier()
-  {
-
-    price.Setgold(15);
-    price.Setfood(20); 
-    sword.setdamage(4);
-    setattackSpeed(0.96);
-    setHealth(30);
-    setagrorange(4);
-    setAgility(1);
-  }
-    public void incrementNumber(){
-   Number++;
-   }
-   public void dcrementNumber(){
-   Number--;
-   }
-   public int  GetNumber(){
-   return Number;
-   }
-  public void setsword (Melee Sword){
-      Sword=sword;
-  }
-  public Melee getsword (){
-      return sword;
-  }
- @Override
-     public String move(Coordinates XY){
-     return"Soldier is moving";
-     }
-      @Override
-     public String attack(){
-     return "Soldier is Attacking";
-     }
-     @Override
-     public String toString() { 
-         return "Soldier";
-      } 
+    Soldier(){
+        price.Setgold(15);
+        price.Setfood(20); 
+        sword.setdamage(4);
+        setattackSpeed(0.96);
+        setHealth(30);
+        setagrorange(4);
+        setAgility(1);
     }
+    
+    public void incrementNumber(){
+        Number++;
+    }
+    
+    public void dcrementNumber(){
+        Number--;
+    }
+    
+    public int  GetNumber(){
+        return Number;
+    }
+    
+    public void setsword (Melee Sword){
+        Sword = sword;
+    }
+    
+    public Melee getsword (){
+        return sword;
+    }
+    
+    @Override
+    public String move(Coordinates XY){
+        return"Soldier is moving";
+    }
+    
+    @Override
+    public String attack(){
+        return "Soldier is Attacking";
+    }
+    
+    @Override
+    public String toString() { 
+        return "Soldier";
+    }
+    
+}
